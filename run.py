841f9026ea1cac493df6f325899071170e6c6980
Arief Riyanto <50206225+ar-42@users.noreply.github.com>
2024-01-05 08:37:32 +0700

    Update run.py
    Penambahan opsi keluar dan pesan setelah melakukan perhitungan

--- run.py
+++ run.py
@@ -17,28 +17,44 @@ def bagi(x, y):
     else:
         return "Error: Pembagian oleh nol"
 
-# Menampilkan menu operasi
-print("Pilih operasi:")
-print("1. Penjumlahan")
-print("2. Pengurangan")
-print("3. Perkalian")
-print("4. Pembagian")
+# Fungsi untuk menampilkan menu operasi
+def tampilkan_menu():
+    print("\nPilih operasi:")
+    print("1. Penjumlahan")
+    print("2. Pengurangan")
+    print("3. Perkalian")
+    print("4. Pembagian")
+    print("5. Keluar")
 
-# Meminta input dari pengguna
-pilihan = input("Masukkan nomor operasi yang diinginkan (1/2/3/4): ")
+# Program Utama
+while True:
+    tampilkan_menu()
 
-# Meminta input bilangan dari pengguna
-bilangan1 = float(input("Masukkan bilangan pertama: "))
-bilangan2 = float(input("Masukkan bilangan kedua: "))
+    # Meminta input dari pengguna
+    pilihan = input("Masukkan nomor operasi yang diinginkan (1/2/3/4/5): ")
 
-# Memproses input pengguna
-if pilihan == '1':
-    print(bilangan1, "+", bilangan2, "=", tambah(bilangan1, bilangan2))
-elif pilihan == '2':
-    print(bilangan1, "-", bilangan2, "=", kurang(bilangan1, bilangan2))
-elif pilihan == '3':
-    print(bilangan1, "*", bilangan2, "=", kali(bilangan1, bilangan2))
-elif pilihan == '4':
-    print(bilangan1, "/", bilangan2, "=", bagi(bilangan1, bilangan2))
-else:
-    print("Input tidak valid")
+    if pilihan == '5':
+        print("Terima kasih. Program selesai.")
+        break
+
+    # Meminta input bilangan dari pengguna
+    bilangan1 = float(input("Masukkan bilangan pertama: "))
+    bilangan2 = float(input("Masukkan bilangan kedua: "))
+
+    # Memproses input pengguna
+    if pilihan == '1':
+        print(bilangan1, "+", bilangan2, "=", tambah(bilangan1, bilangan2))
+    elif pilihan == '2':
+        print(bilangan1, "-", bilangan2, "=", kurang(bilangan1, bilangan2))
+    elif pilihan == '3':
+        print(bilangan1, "*", bilangan2, "=", kali(bilangan1, bilangan2))
+    elif pilihan == '4':
+        print(bilangan1, "/", bilangan2, "=", bagi(bilangan1, bilangan2))
+    else:
+        print("Input tidak valid")
+
+    # Tanya apakah pengguna ingin melakukan perhitungan lagi
+    ulangi = input("Mau melakukan perhitungan lagi? (ya/tidak): ")
+    if ulangi.lower() != 'ya':
+        print("Terima kasih. Program selesai.")
+        break
